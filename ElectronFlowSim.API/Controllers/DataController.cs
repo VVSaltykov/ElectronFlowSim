daa43917e26288284699f89d846d9b9a90ff62b4
SaltykovVV <SaltykovVV@OIT-ZAMENA-04>
2025-07-03 10:59:46 +0400

    fix gitignore

---
+++ ElectronFlowSim.API/Controllers/DataController.cs
@@ -0,0 +1,26 @@
+﻿using ElectronFlowSim.AnalysisService.GRPC.Protos;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Common;
+
+namespace ElectronFlowSim.API.Controllers
+{
+    [ApiController]
+    [Route("api/data/")]
+    public class DataController : ControllerBase
+    {
+        private readonly DBCommunication.DBCommunicationClient dBCommunicationClient;
+
+        public DataController(DBCommunication.DBCommunicationClient dBCommunicationClient)
+        {
+            this.dBCommunicationClient = dBCommunicationClient;
+        }
+
+        [HttpPost("get-drawing-result")]
+        public async Task<IActionResult> CreateSave([FromBody] InputDataDTO inputDataDTO)
+        {
+            var grpcResponse = await dBCommunicationClient.CreateSaveAsync(inputDataDTO);
+
+            return Ok(grpcResponse);
+        }
+    }
+}
---
+++ ElectronFlowSim.AnalysisService.GRPC/Services/DBCommunicationGrpcService.cs
@@ -0,0 +1,56 @@
+﻿using ElectronFlowSim.AnalysisService.Common.Repositories;
+using ElectronFlowSim.AnalysisService.GRPC.Protos;
+using Grpc.Core;
+
+namespace ElectronFlowSim.AnalysisService.GRPC.Services
+{
+    public class DBCommunicationGrpcService : DBCommunication.DBCommunicationBase
+    {
+        private readonly InputDataRepository inputDataRepository;
+
+        public DBCommunicationGrpcService(InputDataRepository inputDataRepository)
+        {
+            this.inputDataRepository = inputDataRepository;
+        }
+
+        public override async Task<EmptyResponse> CreateSave(InputDataDTO _inputDataDTO, ServerCallContext context)
+        {
+            var inputDataDTO = new DTO.AnalysisService.InputDataDTO
+            {
+                ig = _inputDataDTO.Ig,
+                nmas = _inputDataDTO.Nmas,
+                km = _inputDataDTO.Km,
+                kp = _inputDataDTO.Kp,
+                kq = _inputDataDTO.Kq,
+                kpj6 = _inputDataDTO.Kpj6,
+                ik = _inputDataDTO.Ik,
+                j1 = _inputDataDTO.J1,
+                icr = _inputDataDTO.Icr,
+                jcr = _inputDataDTO.Jcr,
+                r = _inputDataDTO.R.ToArray(),
+                z = _inputDataDTO.Z.ToArray(),
+                u = _inputDataDTO.U.ToArray(),
+                l = _inputDataDTO.L.ToArray(),
+                rk = _inputDataDTO.Rk,
+                utep = _inputDataDTO.Utep,
+                zkon = _inputDataDTO.Zkon,
+                akl1 = _inputDataDTO.Akl1,
+                u0 = _inputDataDTO.U0,
+                uekvip = _inputDataDTO.Uekvip.ToArray(),
+                bnorm = _inputDataDTO.Bnorm,
+                abm = _inputDataDTO.Abm,
+                bm = _inputDataDTO.Bm.ToArray(),
+                aik = _inputDataDTO.Aik.ToArray(),
+                ht = _inputDataDTO.Ht,
+                dz = _inputDataDTO.Dz,
+                dtok = _inputDataDTO.Dtok,
+                hq1 = _inputDataDTO.Hq1,
+                ar1s = _inputDataDTO.Ar1S
+            };
+
+            await inputDataRepository.Create(inputDataDTO);
+
+            return new EmptyResponse();
+        }
+    }
+}
