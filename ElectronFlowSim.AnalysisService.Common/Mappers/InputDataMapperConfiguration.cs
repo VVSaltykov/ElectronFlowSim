8164987f4b506f788c0652e1322e927559ff986f
SaltykovVV <SaltykovVV@OIT-ZAMENA-04>
2025-07-15 14:31:12 +0400

    fix

--- ElectronFlowSim.AnalysisService.Common/Mappers/InputDataMapperConfiguration.cs
+++ ElectronFlowSim.AnalysisService.Common/Mappers/InputDataMapperConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
 using ElectronFlowSim.AnalysisService.Domain.Entities;
 using ElectronFlowSim.DTO.AnalysisService;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ElectronFlowSim.AnalysisService.Common.Mappers
 {
@@ -16,6 +11,23 @@ namespace ElectronFlowSim.AnalysisService.Common.Mappers
             CreateMap<InputDataDTO, InputData>();
 
             CreateMap<InputData, InputDataDTO>();
+
+            CreateMap<NZRUTableDTO, NZRUTableData>()
+                .ForMember(dest => dest.N, opt => opt.MapFrom(src => src.N))
+                .ForMember(dest => dest.Z, opt => opt.MapFrom(src => src.Z))
+                .ForMember(dest => dest.R, opt => opt.MapFrom(src => src.R))
+                .ForMember(dest => dest.U, opt => opt.MapFrom(src => src.U))
+                .ForMember(dest => dest.WorkpieceType, opt => opt.MapFrom(src => src.WorkpieceType));
+
+            CreateMap<NLTableDTO, NLTableData>()
+                .ForMember(dest => dest.N, opt => opt.MapFrom(src => src.N))
+                .ForMember(dest => dest.L, opt => opt.MapFrom(src => src.L));
+
+            CreateMap<InputDataForSaveDTO, InputData>()
+                .ForMember(dest => dest.NZRUTableDatas, opt => opt.MapFrom(src => src.NZRUTableDatas))
+                .ForMember(dest => dest.NLTableData, opt => opt.MapFrom(src => src.NLTableData));
+
+            CreateMap<InputData, InputDataForSaveDTO>();
         }
     }
 }
